991c29ddbd00bc9247d5ccb78ca04c2c2f8da6fa
Maxens OLLIVIER <mollivier1@la-joliverie.com>
2019-10-15 11:45:44 -0400

    Itération4_1_SQL_Lieu_modif_longueur_adresse

--- sql/mysql-create-utf8.sql
+++ sql/mysql-create-utf8.sql
@@ -85,7 +85,7 @@ CREATE TABLE Utilisateur (
 CREATE TABLE Lieu(
     Lieu_Id INT(20) AUTO_INCREMENT,
     nom VARCHAR(25),
-    adresse VARCHAR(25),
+    adresse VARCHAR(100),
     capaciteAccueil VARCHAR(25),
     PRIMARY KEY (Lieu_Id)
 );
