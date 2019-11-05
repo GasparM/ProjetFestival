556e6b1b538dcbdc14e68168672d6d0ae9bef99e
GasparMissiaen <gaspar.missiaen@hotmail.fr>
2019-11-05 09:24:38 -0500

    Itération N°4 T7_4 Création test métier lieu

---
+++ test/metier/LieuTest.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Lieu Test</title>
+    </head>
+    <body>
+        <?php
+        use modele\metier\Lieu;
+        require_once __DIR__ . '/../../includes/autoload.inc.php';
+        echo "<h2>Test unitaire de la classe métier Lieu</h2>";
+        $objet = new Lieu(1,"Grosse soirée","24 rue professeur Dubuisson", 200000);
+        var_dump($objet);
+        ?>
+    </body>
+</html>
